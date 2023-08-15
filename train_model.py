e7e35b57a3109516575059d392463bbc37f84cd9
Hks832 <123854827+Hks832@users.noreply.github.com>
2023-08-15 19:39:38 -0400

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -3,7 +3,7 @@ import pandas as pd
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras import optimizers
 from tensorflow.keras.models import Sequential, Model 
-from tensorflow.keras.layers import * 
+from tensorflow.keras.layers import *
 from tensorflow.keras.layers import Flatten
 from tensorflow.keras.layers import GRU, RepeatVector
 from tensorflow.keras.callbacks import ModelCheckpoint, LearningRateScheduler, TensorBoard, EarlyStopping
@@ -14,15 +14,15 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 
 # declare some constants
-data_dir = "Data//" 
+data_dir = "Dataset/" 
 img_height , img_width = 64, 64 # dimension of each frame of videos
 seq_len = 40 # number of images pass as one sequence
 final_seq = int(seq_len/5)
 #print("Final Sequence Length: ", final_seq)
-classes = ["nonvolent", "violent"]
+classes = ["Non-Violent", "Violent"]
 
 # extraction of frames from videos
-#  Creating frames from videos9
+#  Creating frames from videos
 def frames_extraction(video_path):
 	frames_list = []
      
@@ -56,10 +56,10 @@ def create_data(input_dir):
 	for c in classes_list:
 		print(c)
 		if c in classes:
-			if c == "nonviolent":
+			if c == "Non-Violent":
 				y = int(0)
 				#print("*** noFight class ***")
-			elif c == "violent":
+			elif c == "Violent":
 				y = int(1)
 				#print("*** fight class ***")
 			else:
@@ -83,36 +83,41 @@ X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.10, shuffl
 
 
 
-# ConvLSTM based model design
 model = Sequential()
-model.add(ConvLSTM2D(filters = 64, kernel_size = (3, 3), return_sequences = True, data_format = "channels_last", input_shape = (final_seq, img_height, img_width, 3)))
-model.add(ConvLSTM2D(filters = 128, kernel_size = (3, 3), return_sequences = True))
-#model.add(Dropout(0.2))
-model.add(ConvLSTM2D(filters = 256, kernel_size = (3, 3), return_sequences = True))
-#model.add(Flatten())
-#model.add(RepeatVector(1))
-model.add(TimeDistributed(Flatten()))
-model.add(GRU(200))
-#model.add(GlobalAveragePooling2D())
-model.add(Dense(256, activation="relu"))
+
+# First 3D Convolution layer
+model.add(Conv3D(64, kernel_size=(3, 3, 3), activation='relu', input_shape=(final_seq, img_height, img_width, 3)))
+model.add(MaxPooling3D(pool_size=(1, 2, 2), strides=(1, 2, 2)))
+
+# Second 3D Convolution layer
+model.add(Conv3D(128, kernel_size=(3, 3, 3), activation='relu'))
+model.add(MaxPooling3D(pool_size=(1, 2, 2), strides=(1, 2, 2)))
+
+# Third 3D Convolution layer
+model.add(Conv3D(256, kernel_size=(3, 3, 3), activation='relu'))
+model.add(MaxPooling3D(pool_size=(1, 2, 2), strides=(1, 2, 2)))
+
+# Flatten and fully connected layers
+model.add(Flatten())
+model.add(Dense(256, activation='relu'))
 model.add(Dropout(0.5))
-model.add(Dense(128, activation="relu"))
+model.add(Dense(128, activation='relu'))
 model.add(Dropout(0.3))
-model.add(Dense(64, activation="relu"))
+model.add(Dense(64, activation='relu'))
 model.add(Dropout(0.1))
 model.add(Dense(1, activation='sigmoid'))
+
 model.summary()
 
-# training the model
-#opt = keras.optimizers.SGD(lr=0.0001)
+# Training and compiling the model 
 opt = tensorflow.keras.optimizers.Adam(0.0001)
 model.compile(loss='binary_crossentropy', optimizer=opt, metrics=[tensorflow.keras.metrics.BinaryAccuracy()])
 earlystop = EarlyStopping(patience=50)
 callbacks = [earlystop]
 print("[INFO]...Model is training:")
-history = model.fit(x = X_train, y = y_train, epochs=50, batch_size = 8 , shuffle=True, validation_split=0.10, callbacks=callbacks)
+history = model.fit(x = X_train, y = y_train, epochs=50, batch_size = 16 , shuffle=True, validation_split=0.10, callbacks=callbacks)
 
 print("[INFO]...Model is saving:")
-model.save("Surveillance_trained_model.h5")
+model.save("Surveillance_trained_model_3DCNN.h5")
 
 
